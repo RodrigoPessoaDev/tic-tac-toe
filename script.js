f0c23505e005a34b2aacc4100a7a644fce981036
RodrigoPessoaDev <rodrigopessoa.dev@gmail.com>
2024-04-13 15:34:10 +0100

    Reset js code and rewrite functions for game board and players

--- script.js
+++ script.js
@@ -1,81 +1,39 @@
+// const gameBoard = (() => {})();
+
 const gameBoard = (() => {
-  const blankBoard = [0, 0, 0, 0, 0, 0, 0, 0];
-  let gameArray = [0, 0, 0, 0, 0, 0, 0, 0];
+  let arrayBoard = ["X", "X", "", "", "", "", "", "", ""];
+  let playerTurn = "Player One";
 
-  checkRows = () => {
-    let rowOne = gameArray[0] + gameArray[1] + gameArray[2];
-    let rowTwo = gameArray[3] + gameArray[4] + gameArray[5];
-    let rowThree = gameArray[6] + gameArray[7] + gameArray[8];
+  getBoard = () => {
+    return arrayBoard;
+  };
 
-    if (rowOne === "XXX" || rowTwo === "XXX" || rowThree === "XXX") {
-      playerWins();
-    } else if (rowOne === "OOO" || rowTwo === "OOO" || rowThree === "OOO") {
-      computerWins();
-    } else {
-      noWinner();
+  clearBoard = () => {
+    arrayBoard = ["", "", "", "", "", "", "", "", ""];
+    console.log("Board cleared");
+  };
+
+  player1 = () => {
+    return {
+      name: "Player One",
+      mark: "X",
+    };
+  };
+
+  player2 = () => {
+    return {
+      name: "Player Two",
+      mark: "O",
+    };
+  };
+
+  checkTurn = () => {
+    if (playerTurn === player1().name) {
+      playerTurn = "Player Two";
+      console.log("It's Player Two turn.");
+    } else if (playerTurn === player2().name) {
+      playerTurn = "Player One";
+      console.log("It's Player One turn.");
     }
-  };
-
-  checkCols = () => {
-    let colOne = gameArray[0] + gameArray[3] + gameArray[6];
-    let colTwo = gameArray[1] + gameArray[4] + gameArray[7];
-    let colThree = gameArray[2] + gameArray[5] + gameArray[8];
-
-    if (colOne === "XXX" || colTwo === "XXX" || colThree === "XXX") {
-      playerWins();
-    } else if (colOne === "OOO" || colTwo === "OOO" || colThree === "OOO") {
-      computerWins();
-    } else {
-      noWinner();
-    }
-  };
-
-  checkDiag = () => {
-    let diagOne = gameArray[0] + gameArray[4] + gameArray[8];
-    let diagTwo = gameArray[6] + gameArray[4] + gameArray[2];
-
-    if (diagOne === "XXX" || diagTwo === "XXX") {
-      playerWins();
-    } else if (diagOne === "OOO" || diagTwo === "OOO") {
-      computerWins();
-    } else {
-      noWinner();
-    }
-  };
-
-  // Winning
-
-  playerWins = () => {
-    console.log("Player Wins");
-  };
-
-  computerWins = () => {
-    console.log("Computer Wins");
-  };
-
-  noWinner = () => {
-    console.log("Keep Playing");
-  };
-
-  // Players
-
-  createPlayers = () => {
-    return [
-      {
-        name: "Player",
-        mark: "X",
-      },
-
-      {
-        name: "Computer",
-        mark: "O",
-      },
-    ];
-  };
-
-  getComputerChoice = () => {
-    let computerChoice = Math.floor(Math.random() * 9);
-
-    return computerChoice;
   };
 })();
